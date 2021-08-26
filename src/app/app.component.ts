cc04867853694ef1767ebf355248f72276fda5a3
Thanadit Buthong <thanadit.buthong@accenture.com>
2021-08-26 09:53:33 +0700

    side menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,26 +9,5 @@ import { BrowserStorageService } from './browser-storage.service';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'ReactiveForms';
-  bmi = 0;
-  info = '';
-
-  weightForm = new FormControl(30, Validators.required);
-  heightForm = new FormControl(1.4, Validators.required);
-
-  constructor() {}
-
-  // A BMI of 25.0 or more is overweight, while the healthy range is 18.5 to 24.9. BMI applies to most adults 18-65 years.
-
-  calculate() {
-    if (this.weightForm.invalid || this.heightForm.invalid) {
-      return;
-    }
-    const weight = this.weightForm.value;
-    const height = this.heightForm.value;
-    console.log('weight', weight);
-    console.log('height', height);
-
-    this.bmi = weight / height ** 2;
-  }
+  active = true;
 }
